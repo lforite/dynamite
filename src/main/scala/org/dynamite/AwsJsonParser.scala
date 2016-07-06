da8a22ceaece91ea5e6eb5f3f140575d1036b796
louis.forite <forite.louis@gmail.com>
2016-07-06 23:50:33 +0200

    Adding support for L fields

--- src/main/scala/org/dynamite/AwsJsonParser.scala
+++ src/main/scala/org/dynamite/AwsJsonParser.scala
@@ -6,22 +6,18 @@ trait AwsJsonParser {
 
   def fromAws(json: JValue): JValue = shrinkObject(json)
 
-  def toAws(json: JValue): JValue = ???
-
-  private def shrinkField(t: (String, JValue)): (String, JValue) = {
-    t match {
-      case (name, JObject(List(JField("S", s: JString)))) => (name, s)
-      case (name, JObject(List(JField("N", s: JString)))) => (name, s)
-      case (name, JObject(List(JField("BOOL", s: JBool)))) => (name, s)
-      case (name, JObject(List(JField("SS", s: JArray)))) => (name, s)
-    }
+  private def shrinkField(field: (String, JValue)): (String, JValue) = field match {
+    case (name, JObject(List(JField("S", s: JString)))) => (name, s)
+    case (name, JObject(List(JField("N", s: JString)))) => (name, s)
+    case (name, JObject(List(JField("BOOL", s: JBool)))) => (name, s)
+    case (name, JObject(List(JField("SS", s: JArray)))) => (name, s)
+    case (name, JObject(List(JField("L", s: JArray)))) => (name, s map shrinkObject)
   }
 
-  private def shrinkObject(json: JValue): JValue = {
-    json match {
-      case JObject(l) => JObject(l map shrinkField)
-      case JArray(arr) => JArray(arr map shrinkObject)
-    }
+  private def shrinkObject(json: JValue): JValue = json match {
+    case JObject(l) => JObject(l map shrinkField)
+    case JArray(arr) => JArray(arr map shrinkObject)
+    case value: JValue => value
   }
 
 }
--- src/test/scala/org/dynamite/AwsJsonParserTest.scala
+++ src/test/scala/org/dynamite/AwsJsonParserTest.scala
@@ -15,18 +15,21 @@ class AwsJsonParserTest extends Specification {
     val nField: JValue = JObject(List(JField("N", JString("n value"))))
     val boolField: JValue = JObject(List(JField("BOOL", JBool(true))))
     val ssField: JValue = JObject(List(JField("SS", JArray(List(JString("s1"), JString("s2"))))))
+    val lField: JValue = JObject(List(JField("L", JArray(List(JInt(1), JInt(2))))))
 
     val aws: JValue = JObject(List(
       JField("sField", sField),
       JField("nField", nField),
       JField("bField", boolField),
-      JField("ssField", ssField)))
+      JField("ssField", ssField),
+      JField("lField", lField)))
 
     val expected: JValue = JObject(List(
       JField("sField", JString("s value")),
       JField("nField", JString("n value")),
       JField("bField", JBool(true)),
-      JField("ssField", JArray(List(JString("s1"), JString("s2"))))))
+      JField("ssField", JArray(List(JString("s1"), JString("s2")))),
+      JField("lField", JArray(List(JInt(1), JInt(2))))))
 
     Dummy.fromAws(aws) must be_==(expected)
   }
