9f503a2acc942a801cba0eb34956370cb475a84a
louis.forite <forite.louis@gmail.com>
2016-07-28 21:56:49 +0200

    Adding unreachable host error

--- src/main/scala/org/dynamite/dsl/DynamoError.scala
+++ src/main/scala/org/dynamite/dsl/DynamoError.scala
@@ -5,6 +5,7 @@ import javax.crypto.spec.SecretKeySpec
 /** more info at http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/CommonErrors.html */
 sealed trait DynamoError
 case class BasicDynamoError() extends DynamoError
+case class UnreachableHostException(host: String) extends DynamoError
 case object JsonSerialisationError extends DynamoError
 
 case object IncompleteSignatureError extends DynamoError
--- src/main/scala/org/dynamite/http/HttpClient.scala
+++ src/main/scala/org/dynamite/http/HttpClient.scala
@@ -1,9 +1,11 @@
 package org.dynamite.http
 
+import java.net.ConnectException
+
 import com.ning.http.client.Response
 import dispatch.Defaults._
 import dispatch._
-import org.dynamite.dsl.{BasicDynamoError, DynamoError}
+import org.dynamite.dsl.{BasicDynamoError, DynamoError, UnreachableHostException}
 
 import scala.concurrent.Future
 import scalaz.EitherT
@@ -12,6 +14,7 @@ import scalaz.Scalaz._
 trait HttpClient {
   protected[dynamite] def httpRequest(req: Req): EitherT[Future, DynamoError, Response] = {
     EitherT.fromEither(Http(req).either) leftMap {
+      case ce: ConnectException => UnreachableHostException(req.url)
       case _: Throwable => BasicDynamoError()
     }
   }
