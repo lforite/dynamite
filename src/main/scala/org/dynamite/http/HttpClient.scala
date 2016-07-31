62766fd8e642561a7129cf06cbb8a0cb988bb34d
louis.forite <forite.louis@gmail.com>
2016-07-31 12:04:04 +0200

    #5 feat: adding implicit excecution context to run in a separate context http requests

--- src/main/scala/org/dynamite/http/HttpClient.scala
+++ src/main/scala/org/dynamite/http/HttpClient.scala
@@ -3,16 +3,15 @@ package org.dynamite.http
 import java.net.ConnectException
 
 import com.ning.http.client.Response
-import dispatch.Defaults._
 import dispatch._
 import org.dynamite.dsl.{BasicDynamoError, DynamoError, UnreachableHostException}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scalaz.EitherT
 import scalaz.Scalaz._
 
 trait HttpClient {
-  protected[dynamite] def httpRequest(req: Req): EitherT[Future, DynamoError, Response] = {
+  protected[dynamite] def httpRequest(req: Req)(implicit ex: ExecutionContext): EitherT[Future, DynamoError, Response] = {
     EitherT.fromEither(Http(req).either) leftMap {
       case ce: ConnectException => UnreachableHostException(req.url)
       case _: Throwable => BasicDynamoError()
