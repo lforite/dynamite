d15bb0df45a191b534c54aa3f3d7790afc230e0f
louis.forite <forite.louis@gmail.com>
2016-11-12 22:20:58 +0100

    issue-36 adding all the aws types

--- src/main/scala/org/dynamite/ast/AwsTypes.scala
+++ src/main/scala/org/dynamite/ast/AwsTypes.scala
@@ -6,8 +6,45 @@ import org.json4s.JsonAST.{JField, JObject, JString}
 sealed trait AwsType
 sealed trait AwsScalarType extends AwsType
 
+/**
+  * An attribute of type String
+  */
 case class S(value: String) extends AwsScalarType
+
+/**
+  * An attribute of type Number
+  */
 case class N(value: String) extends AwsScalarType
+
+/**
+  * An attribute of type Boolean
+  */
+case class BOOL(boolean: Boolean) extends AwsScalarType
+
+/**
+  * An attribute of type List
+  */
+case class L(elements: AwsType) extends AwsType
+
+/**
+  * An attribute of type Map
+  */
+case class M(elems: (String, AwsType)*) extends AwsType
+
+/**
+  * An attribute of type Number
+  */
+case class NS(numbers: Set[N]) extends AwsType
+
+/**
+  * An attribute of type Null
+  */
+case object NULL extends AwsType
+
+/**
+  * An attribute of type String Set
+  */
+case class SS(strings: Set[S]) extends AwsType
 
 private[dynamite] class AwsTypeSerializer extends CustomSerializer[AwsType](format => ( {
   case JObject(List(JField("S", JString(value)))) => S(value)
