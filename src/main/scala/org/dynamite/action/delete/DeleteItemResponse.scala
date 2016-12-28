eb3509bda8b5405670a7baac32813b1920680cc1
louis.forite <forite.louis@gmail.com>
2016-12-28 18:07:52 +0100

    issue-20 value classes now extend anyval

--- src/main/scala/org/dynamite/action/delete/DeleteItemResponse.scala
+++ src/main/scala/org/dynamite/action/delete/DeleteItemResponse.scala
@@ -3,7 +3,7 @@ package org.dynamite.action.delete
 import org.dynamite.dsl.JsonDeserializable
 import org.json4s._
 
-private[dynamite] case class DeleteItemResponse(attributes: JValue)
+private[dynamite] case class DeleteItemResponse(attributes: JValue) extends AnyVal
 
 private[dynamite] object DeleteItemResponse {
   implicit val fromJson = new JsonDeserializable[DeleteItemResponse] {
